fa95efe8ce8a60c3c9a994294d37f98ab4e55d3a
Youngjun Ha <dudwnsk011@gmail.com>
2022-07-19 18:01:20 +0900

    second commit

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,14 @@
+import { useRouter } from 'next/router';
+
 function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className='center'>Loading</p>
+  }
+
   return (
     <div>
       <h1>Filtered Events</h1>
